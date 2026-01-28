8e62fda0191d977a685031c4468cdfb903799e06
Karen Mamani <salaskaren035@gmail.com>
2026-01-28 14:34:30 -0400

    deploy automatico

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 import mysql.connector
 import bcrypt
-from flask import Flask, render_template
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
 
 
 app = Flask(__name__)
